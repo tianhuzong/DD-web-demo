dc548b42b60830c8e1b3f0c22c1a6afa21921ba7
thzsen <tianhuzong@qq.com>
2024-02-07 18:04:53 +0800

    更新了文档,dockerfile,DD_tools,requirements.txt,删除了一部分不必要的文件

--- DD-webui/DD_tools.py
+++ DD-webui/DD_tools.py
@@ -1,5 +1,9 @@
-import deepdanbooru as dd
 import os
+import tensorflow as tf
+import tensorflow_io as tfio
+import numpy as np
+import math
+import skimage.transform
 def get_mark(img_path,model,tags,yz,remove):
     """
     对一个图片评估标签
@@ -11,7 +15,7 @@ def get_mark(img_path,model,tags,yz,remove):
         remove:评估完是否删除图片
     """
     markdict = {}
-    for tag,mark in dd.commands.evaluate_image(img_path, model, tags, yz):
+    for tag,mark in evaluate_image(img_path, model, tags, yz):
         markdict[tag] = str(mark)
         #print(f"{tag}:{mark}")
     if remove:
@@ -27,3 +31,97 @@ def get_files_in_folder(folder_path):
             file_path = os.path.join(root, file)
             file_list.append(file_path)
     return file_list
+
+
+def transform_and_pad_image(
+    image,
+    target_width,
+    target_height,
+    scale=None,
+    rotation=None,
+    shift=None,
+    order=1,
+    mode="edge",
+):
+    """
+    Transform image and pad by edge pixles.
+    """
+    image_width = image.shape[1]
+    image_height = image.shape[0]
+    image_array = image
+
+    # centerize
+    t = skimage.transform.AffineTransform(
+        translation=(-image_width * 0.5, -image_height * 0.5)
+    )
+
+    if scale:
+        t += skimage.transform.AffineTransform(scale=(scale, scale))
+
+    if rotation:
+        radian = (rotation / 180.0) * math.pi
+        t += skimage.transform.AffineTransform(rotation=radian)
+
+    t += skimage.transform.AffineTransform(
+        translation=(target_width * 0.5, target_height * 0.5)
+    )
+
+    if shift:
+        t += skimage.transform.AffineTransform(
+            translation=(target_width * shift[0], target_height * shift[1])
+        )
+
+    warp_shape = (target_height, target_width)
+
+    image_array = skimage.transform.warp(
+        image_array, (t).inverse, output_shape=warp_shape, order=order, mode=mode
+    )
+
+    return image_array
+
+def load_image_for_evaluate(
+    input_: str, width: int, height: int, normalize: bool = True
+) -> Any:
+    if isinstance(input_, six.BytesIO):
+        image_raw = input_.getvalue()
+    else:
+        image_raw = tf.io.read_file(input_)
+    try:
+        image = tf.io.decode_png(image_raw, channels=3)
+    except:
+        image = tfio.image.decode_webp(image_raw)
+        image = tfio.experimental.color.rgba_to_rgb(image)
+
+    image = tf.image.resize(
+        image,
+        size=(height, width),
+        method=tf.image.ResizeMethod.AREA,
+        preserve_aspect_ratio=True,
+    )
+    image = image.numpy()  # EagerTensor to np.array
+    image = transform_and_pad_image(image, width, height)
+
+    if normalize:
+        image = image / 255.0
+
+    return image
+def evaluate_image(
+    image_input: str, model: Any, tags: list, threshold: float
+) -> tuple:
+    width = model.input_shape[2]
+    height = model.input_shape[1]
+
+    image = load_image_for_evaluate(image_input, width=width, height=height)
+
+    image_shape = image.shape
+    image = image.reshape((1, image_shape[0], image_shape[1], image_shape[2]))
+    y = model.predict(image)[0]
+
+    result_dict = {}
+
+    for i, tag in enumerate(tags):
+        result_dict[tag] = y[i]
+
+    for tag in tags:
+        if result_dict[tag] >= threshold:
+            yield tag, result_dict[tag]
