dbf524f0c4effecb7061eef6d9a510571151ead4
thzsen <tianhuzong@qq.com>
2024-04-04 13:17:35 +0800

    彻底删除对deepdanbooru模块的依赖

--- DD-webui/deepdanbooru/model/__init__.py
+++
@@ -1,8 +0,0 @@
-import deepdanbooru.model.layers
-import deepdanbooru.model.losses
-
-from .resnet import create_resnet_152
-from .resnet import create_resnet_custom_v1
-from .resnet import create_resnet_custom_v2
-from .resnet import create_resnet_custom_v3
-from .resnet import create_resnet_custom_v4
--- DD-webui/deepdanbooru/project/__init__.py
+++
@@ -1,4 +0,0 @@
-from .project import DEFAULT_PROJECT_CONTEXT
-from .project import load_project
-from .project import load_model_from_project
-from .project import load_tags_from_project
