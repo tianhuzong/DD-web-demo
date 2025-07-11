a33dd3821ba3abbeef221a37f0852d27b531063a
仙 <110833020+tianhuzong@users.noreply.github.com>
2025-07-11 10:15:47 +0800

    尝试让他不要输出日志而失败，然后修改predict函数的介绍，简化代码，当没有命令行参数时不加载模型

--- DD-webui/cli.py
+++ DD-webui/cli.py
@@ -1,27 +1,6 @@
 import click
-import tensorflow as tf
-import DD_tools
-import logging
-logging.getLogger("tensorflow").setLevel(logging.ERROR)
-tf.config.set_soft_device_placement(True)
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-
-
-gpus = tf.config.experimental.list_physical_devices('GPU')
-
-# 如果有 GPU 可用，启用 GPU 并设置动态分配内存
-if gpus:
-    try:
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-        print("Using GPU for model loading and training.")
-    except RuntimeError as e:
-        print(e)
-else:
-    print("No GPU available. Using CPU instead.")
-
-model = tf.keras.models.load_model("./model/model-resnet_custom_v3.h5", compile=False)
-tags = DD_tools.load_tags("./model/tags.txt")
+import warnings
+warnings.simplefilter("ignore")
 
 @click.group()
 def cli():
@@ -32,10 +11,20 @@ def cli():
 @click.argument('photo_path',  type=str)
 def predict(photo_path):
     """
-    Predict the survival of a passenger based on their input data\n
+    Predict the tags of a photo\n
     The input arg is photo_path
+    Usage:
+        python cli.py predict <photo_path>
     """
-    click.echo(DD_tools.get_mark(photo_path, model, tags, 0.5, False))
+    click.echo("Loading model...")
+
+    import tensorflow as tf
+    import DD_tools
+    model = tf.keras.models.load_model("./model/model-resnet_custom_v3.h5", compile=False)
+
+    print("Done...\nPredicting...")
+    tags = DD_tools.load_tags("./model/tags.txt")
+    click.echo(DD_tools.get_mark(photo_path, model, tags, 0.75, False))
 
 
     
